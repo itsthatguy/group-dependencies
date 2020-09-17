cf8a0d111fe1c0bce35ca6e3771187c6c5751d22
Amin Yahyaabadi <aminyahyaabadi74@gmail.com>
2020-09-16 21:56:27 -0500

    test for mixed dependencies

--- __tests__/deps.js
+++ __tests__/deps.js
@@ -30,12 +30,20 @@ describe('deps', function () {
     expect(out).toContain("No buildDependencies found.");
   });
 
-  it('installs group dependencies', function () {
+  it('installs test group dependencies', function () {
     deps('install', 'test');
     const npmCmd = process.platform === 'win32' ? 'npm.cmd' : 'npm'
     expect(out).toEqual('deps info resolve jest@^26.4.2\n' +
                         'deps warn resolve @babel/cli not found: installing latest\n' +
                         'deps cmd npm install jest@^26.4.2 @babel/cli\n' +
                         `${npmCmd} install jest@^26.4.2 @babel/cli\n`);
+  });
+
+  it('installs mixed group dependencies', function () {
+    deps('install', 'mixed');
+    expect(out).toEqual('deps info resolve jest@"^26.4.2"\n' +
+                        'deps info resolve colors@"^1.4.0"\n' +
+                        'deps cmd npm install jest@"^26.4.2" colors@"^1.4.0"\n' +
+                        'npm install jest@"^26.4.2" colors@"^1.4.0"\n');
   });
 });
